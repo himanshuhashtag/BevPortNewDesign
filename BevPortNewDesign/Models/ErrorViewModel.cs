b8572dca6c4ee5fc293a3a396351fb7c9d6ad861
dell <dell@DESKTOP-RE97V04>
2023-05-30 14:47:26 +0530

    New Code

---
+++ BevPortNewDesign/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BevPortNewDesign.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
